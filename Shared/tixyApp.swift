bbaffdc8f3207642bdde037c1cdb7675431b6caa
khoi <git@khoi.io>
2020-12-17 14:23:45 +0700

    Initial Commit

---
+++ Shared/tixyApp.swift
@@ -0,0 +1,17 @@
+//
+//  tixyApp.swift
+//  Shared
+//
+//  Created by khoi on 12/17/20.
+//
+
+import SwiftUI
+
+@main
+struct tixyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
